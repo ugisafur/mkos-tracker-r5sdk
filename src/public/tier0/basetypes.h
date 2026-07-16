f9b5f50bb961f7a99fe26ccfa18b68ad9c67ead3
ugisafur <234134352+ugisafur@users.noreply.github.com>
2026-07-16 20:22:56 +0300

    bump sdk version

--- src/public/tier0/basetypes.h
+++ src/public/tier0/basetypes.h
@@ -141,7 +141,7 @@
 #define MAX_MAP_NAME      64 // Max BSP file name len.
 #define MAX_PLAYLIST_NAME 64 // Max playlist name len.
 
-#define SDK_VERSION "VGameSDK019" // Increment this with every /breaking/ SDK change (i.e. security/backend changes breaking compatibility).
+#define SDK_VERSION "VGameSDK020" // Increment this with every /breaking/ SDK change (i.e. security/backend changes breaking compatibility).
 #define SDK_ARRAYSIZE(arr) ((sizeof(arr) / sizeof(*arr))) // Name due to IMGUI implementation and NT implementation that we shouldn't share across everywhere.
 
 #define SDK_SYSTEM_CFG_PATH "cfg/system/"
